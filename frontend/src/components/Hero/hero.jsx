aced25e8f851a35367e06cde2f6fac53f9d8593b
oditha4523 <odithachamod@gmail.com>
2025-11-09 14:27:29 +0000

    initial commit

---
+++ frontend/src/components/Hero/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='main-container'>
+      <div className="hero-container">
+        <h1>Welcome to Our Website</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
