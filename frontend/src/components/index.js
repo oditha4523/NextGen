ace0e90017c6a5f3997a68b49a533f5e81b7676d
oditha4523 <odithachamod@gmail.com>
2025-11-12 07:47:19 +0000

    Add new sections: CTA, Counter, Hero, Services, Testimonials, and What We Do with corresponding styles and functionality

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,14 +1,14 @@
 // Layout Components
-export { default as Header } from './layout/Header';
-export { default as Footer } from './layout/Footer';
+export { default as Header } from './layout/Header/Header';
+export { default as Footer } from './layout/Footer/Footer';
 
 // Section Components  
-export { default as HeroSection } from './sections/HeroSection';
-export { default as ServicesSection } from './sections/ServicesSection';
-export { default as AboutSection } from './sections/AboutSection';
-export { default as WhatWeDoSection } from './sections/WhatWeDoSection';
-export { default as ApproachSection } from './sections/ApproachSection';
-export { default as CounterSection } from './sections/CounterSection';
-export { default as TestimonialsSection } from './sections/TestimonialsSection';
-export { default as BlogSection } from './sections/BlogSection';
-export { default as CTASection } from './sections/CTASection';
+export { default as HeroSection } from './sections/HeroSection/HeroSection';
+export { default as ServicesSection } from './sections/ServicesSection/ServicesSection';
+export { default as AboutSection } from './sections/AboutSection/AboutSection';
+export { default as WhatWeDoSection } from './sections/WhatWeDoSection/WhatWeDoSection';
+export { default as ApproachSection } from './sections/ApproachSection/ApproachSection';
+export { default as CounterSection } from './sections/CounterSection/CounterSection';
+export { default as TestimonialsSection } from './sections/TestimonialsSection/TestimonialsSection';
+export { default as BlogSection } from './sections/BlogSection/BlogSection';
+export { default as CTASection } from './sections/CTASection/CTASection';
